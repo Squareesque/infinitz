8dacedb1a1f64aeb4cb16ae611024397027f4dce
Krzysztof Sajkowski <248864@student.pwr.edu.pl>
2022-06-01 17:20:23 +0200

    Initial commit

---
+++ src/main/java/io/github/squareesque/infinitz/InfinitzApplication.java
@@ -0,0 +1,13 @@
+package io.github.squareesque.infinitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinitzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/squareesque/infinitz/InfinitzApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.squareesque.infinitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
